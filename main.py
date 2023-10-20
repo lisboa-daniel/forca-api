8994a7ffa0d540a447b2b6691b71f79eecd00dca
Vinicius Lima de Castro Padilha <vlcp197@gmail.com>
2023-10-20 20:24:05 -0300

    Inclusão dos métodos de registro de usuário
    E getters e setters de ícone

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ import sqlite3
 
 app = Flask(__name__)
 
-# SQLite database connection
 conn = sqlite3.connect('data.db')
 cursor = conn.cursor()
 
-# API endpoint to handle Unity WebGL requests
 @app.route('/api/login', methods=['POST'])
 def login():
     try:
@@ -23,13 +21,74 @@ def login():
         user = cursor.fetchone()
 
         if user:
-            return jsonify({"message": "Login Successful"})
+            return jsonify({"message": "Login Successful"}), 200
         else:
             return jsonify({"error": "Invalid username or password"}), 401
         
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-# Run the Flask app
+@app.route('api/register', methods=['POST'])
+def register():
+    try:
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+    
+        cursor.execute(f"""SELECT * 
+                           FROM tb_user 
+                           WHERE username='{username}'
+                           """)
+        user = cursor.fetchone()
+
+        if user:
+            return jsonify({"message": "User already exists"}), 200
+        else:
+            cursor.execute(
+                f"""INSERT INTO tb_user (username,
+                                        password, 
+                                        icon, 
+                                        coins, 
+                                        level, 
+                                        xp)
+                    VALUES ('{username}',
+                            '{password}',
+                            '0',0,0,0)""")
+            return jsonify({"message": "User Successfully Registered"}), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('api/set_icon', methods=['UPDATE'])
+def set_icon():
+
+    username = data['username']
+    icon = data['icon']
+    try:
+        cursor.execute(f"""UPDATE tb_user 
+                        SET icon = '{icon}'
+                        WHERE username = '{username}'
+                        """)
+        return jsonify({"message": "Icon Updated"})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('api/get_icon', methods=['POST'])
+def get_icon():
+    username = data['username']
+    try:
+        cursor.execute(f"""SELECT icon 
+                           FROM tb_user 
+                           WHERE username='{username}'
+                           """)
+        icon = cursor.fetchone()
+        if icon:
+            icon_binary = icon[0]
+            return (send_file(io.BytesIO(icon_binary), mimetype='image/png', as_attachment=True, download_name=f'icon.png'))
+        else:
+            return jsonify({"message":"icon not found" }), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 if __name__ == '__main__':
     app.run(debug=True)
