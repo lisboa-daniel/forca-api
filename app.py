40bba9d508f2d3d0fd1a430dc31ffb48aff55fe5
lisboa-daniel <daniel.lisboa.sp@hotmail.com>
2023-10-24 17:50:27 -0300

    character added

--- app.py
+++ app.py
@@ -14,9 +14,12 @@ def connect_db():
         print(f"Error connecting to the database: {e}")
         return None
 
+conn = connect_db()
+cursor = conn.cursor()
+
 # Update your route functions to use PostgreSQL
 
-
+# CADASTRO DE USUARIO
 @app.route('/api/login', methods=['POST'])
 def login():
     conn = connect_db()
@@ -42,7 +45,6 @@ def login():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-# /api/register
 @app.route('/api/register', methods=['POST'])
 def register():
     conn = connect_db()
@@ -51,7 +53,8 @@ def register():
         data = request.get_json()
         username = data['username']
         password = data['password']
-    
+        color = data.get('color', 'blue')  # Default color is blue if not provided
+
         cursor.execute("""SELECT * 
                         FROM tb_user 
                         WHERE username=%s""", (username,))
@@ -61,14 +64,22 @@ def register():
             return jsonify({"message": "User already exists"}), 200
         else:
             cursor.execute(
-                """INSERT INTO tb_user (username, password, icon, coins, level, xp)
-                VALUES (%s, %s, %s, %s, %s, %s)""", (username, password, '0', 0, 0, 0))
+                """INSERT INTO tb_user (username, password, icon, coins, level, xp, item1,item2,item3)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)""", 
+                (username, password, '0', 300, 1, 0,"potion","lapiseira","sopa"))
+            
+            cursor.execute(
+                """INSERT INTO tb_character (username, color, head, top, bottom)
+                VALUES (%s, %s, %s, %s, %s)""", 
+                (username, color, 'default', 'default', 'default'))
+            
             conn.commit()
             conn.close()
-            return jsonify({"message": "User Successfully Registered"}), 200
+            return jsonify({"message": "User and Character Successfully Registered"}), 200
         
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
 
 # /api/user
 @app.route('/api/user', methods=['GET'])
@@ -89,12 +100,15 @@ def get_users():
             "icon": item[3],
             "coins": item[4],
             "level": item[5],
-            "xp": item[6]
+            "xp": item[6],
+            "item1": item[7],
+            "item2": item[8],
+            "item3": item[9]
         }
         users.append(user)
 
     # Return as JSON
-    return json.dumps(users)
+    return jsonify(users)
 
 # /api/userbynick/<string:username>
 @app.route('/api/userbynick/<string:username>', methods=['GET'])
@@ -114,13 +128,57 @@ def get_userbynick(username):
             "icon": item[3],
             "coins": item[4],
             "level": item[5],
-            "xp": item[6]
+            "xp": item[6],
+            "item1": item[7],
+            "item2": item[8],
+            "item3": item[9]
         }
         # Return the user as JSON
-        return json.dumps(user)
+        return jsonify(user)
     else:
         # If the user doesn't exist, return an appropriate error message
-        return json.dumps({"error": "User not found"})
+        return jsonify({"error": "User not found"})
+
+# INFO PERSONAGEM
+# Character Methods
+@app.route('/api/get_color/', methods=['POST'])
+def get_color():
+    conn = connect_db()
+    cursor = conn.cursor()
+    data = request.get_json()
+    username = data['username']
+    query = """SELECT color
+               FROM tb_character
+               WHERE username = %s
+            """
+    try:
+        cursor.execute(query, (username,))
+        color = cursor.fetchone()
+        if color:
+            color_value = color[0]
+            return jsonify({"message": str(color_value)}), 200
+        else:
+            return jsonify({"message": "color not found"}), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/api/set_color', methods=['POST'])
+def set_color():
+    conn = connect_db()
+    cursor = conn.cursor()
+    data = request.get_json()
+    username = data['username']
+    color = data['color']
+    query= """UPDATE tb_character 
+              SET color = %s
+              WHERE username = %s
+            """
+    try:
+        cursor.execute(query, (color, username))
+        conn.commit()
+        return jsonify({"message": "color updated"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
